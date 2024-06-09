c0a16c5bb7e467bd9cc9d50e8ab2e049b14ccdf2
Jonathan Yi <jhyunbinyi@gmail.com>
2024-06-08 19:36:06 -0700

    text spacing

--- src/Components/TextOne.svelte
+++ src/Components/TextOne.svelte
@@ -2,6 +2,7 @@
 <p class="body-text">
 <br>
 The 1990s were a golden age for basketball. The league was expanding and in prime position to gain recognition worldwide. The Chicago Bulls, led by a dominant Michael Jordan, grasped this opportunity and created a dynasty, winning over six championships in nine years. After Jordan retired, many other young players were touted as the next Michael Jordan, and for every player like Kobe Bryant who could live up to this title, fifty more couldn't. To determine whether Anthony Edwards and Michael Jordan are truly similar, we must first define what makes two players similar. The problem here is that every player is unique. Players are an amalgamation of tons of characteristics. No two players are exactly the same. So, how are we going to compare MJ and Ant? For the sake of this project, we will look at these players as they are in-game, ignoring intangibles. That means that we have to access how these players are similar on the basketball court. To do this we will assess what kinds of shots these players take. Where do they like shooting from? Where on the court do they get most of their success? These are the questions we will first look to answer. After that we will access if their shots result in similar levels of scoring. These guys are known scorers, so do they score at the same level across similar stages of their careers? Through the combination of these questions we hope to answer the question of how similar Michael Jordan and Anthony Edwards really are.
+<br><br><br>
 </p>
 
 <style>
